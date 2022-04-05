aae224472884bfc90043f4bbe1e18b1d0761aeff
qmdl980 <qmdl98056@gmail.com>
2022-04-05 16:54:26 +0900

    수정, 삭제 작업 완료

--- src/controller/ArticleController.ts
+++ src/controller/ArticleController.ts
@@ -25,4 +25,32 @@ export class ArticleController{
         const article = await getConnection().getRepository(Article).findOne({where: {id: id}});
         res.send(article);
     }
+
+    static editArticle = async (req, res) => {
+        const {id, title, text, category} = req.body
+
+        const updateOption = {}
+        if(title){
+            updateOption['title'] = title
+        }
+        if(text){
+            updateOption['text'] = text
+        }
+        if(category){
+            updateOption['category'] = category
+        }
+        const result = await getConnection().createQueryBuilder().update(Article)
+            .set(updateOption)
+            .where("id= :id", {id})
+            .execute()
+
+        res.send(result)
+    }
+
+    static deleteArticle = async (req, res) => {
+        const {id} = req.query
+
+        const result = await getConnection().createQueryBuilder().delete().from(Article).where("id= :id", {id}).execute()
+        res.send(result)
+    }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,8 @@ import {CategoryController} from "../controller/CategoryController";
 const routes = Router();
 
 routes.post('/write_article', ArticleController.addArticle);
+routes.put('/write_article', ArticleController.editArticle);
+routes.delete('/article',ArticleController.deleteArticle);
 routes.get('/articles', ArticleController.findAllArticle);
 routes.get('/article/:id', ArticleController.findOneArticle);
 routes.get('/category', CategoryController.findAllCategory);
